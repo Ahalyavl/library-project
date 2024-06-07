40a89b3708a9d104a2236e8057bb368ebe1340e6
thenameisahalya <ahalyavl24@gmail.com>
2024-06-07 21:50:22 +0530

    project done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
         <nav className="navbar navbar-expand-lg bg-primary">
   <div className="container-fluid">
-    <Link className="navbar-brand" href="/">AddBook</Link>
+    <Link className="navbar-brand" to="/">AddBook</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,81 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewAll = () => {
-  return (
-    <div>
-        <Navbar/>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">Title</th>
-      <th scope="col">Author</th>
-      <th scope="col">Pulished Year</th>
-      <th scope="col">price</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    <tr>
-      <th scope="row">Alchemist</th>
-      <td>Paulo cohelo</td>
-      <td>2002</td>
-      <td>888</td>
-    </tr>
-    
-  </tbody>
-</table>
+    const [data, changeData] = useState(
+        [
+            { "title": "Alchemist", "Author": "paulo cohelo", "price": 999 },
+            { "title": "Halfgirlfriend", "Author": "chetan bhagat", "price": 555 },
+            { "title": "devdas", "Author": "sarat chandra", "price": 888 }
 
+
+
+
+
+        ]
+    )
+
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">title</th>
+                                    <th scope="col">Author</th>
+                                    
+                                    <th scope="col">price</th>
+                                </tr>
+                            </thead>
+
+                            {data.map(
+                                (value,index)  => {
+                            return  <tbody>
+                                <tr>
+                                    <th scope="row">{value.title}</th>
+                                    <td>{value.Author}</td>
+                                    
+                                    <td>{value.price}</td>
+                                </tr>
+
+
+                            </tbody>
+
+                         
+                        }
+                    )}
+
+
+                        </table>
+
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ViewAll
