0958b8a24380249e0b1abe3c08bd72692d2e00bf
thenameisahalya <ahalyavl24@gmail.com>
2024-06-07 15:35:53 +0530

    Project Done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg bg-primary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" href="/">AddBook</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/search">SearchBook</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/delete">DeleteBook</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/viewAll">ViewAll</Link>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Pulished Year</th>
+      <th scope="col">price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    <tr>
+      <th scope="row">Alchemist</th>
+      <td>Paulo cohelo</td>
+      <td>2002</td>
+      <td>888</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default ViewAll
