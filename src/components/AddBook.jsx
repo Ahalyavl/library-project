b52dcd013daae242440ac83d75aab75775e0b645
thenameisahalya <ahalyavl24@gmail.com>
2024-06-10 23:05:52 +0530

    project done

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,65 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const AddBook = () => {
-  return (
-    <div>
-      <Navbar/>  
-<div className="container">
-    <div className="row">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-        <div className="row g-3">
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <label htmlFor="" className="form-label">BOOK TITLE</label>
-                <input type="text" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Category</label>
-            <select name="" id="" className="form-control">
-                <option value="">Fiction</option>
-                <option value="">Biography</option>
-                <option value="">Science</option>
-            </select>
+    const [data, setData] = useState({
+        bookTitle: "",
+        author: "",
+        publishedyear: "",
+        edition: "",
+        category: "",
+        
+        price: ""
+       
+    })
 
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Published Year</label>
-            <input type="date" name="" id="" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Author</label>
-            <input type="text" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Description</label>
-            <textarea name="" id="" className="form-control"></textarea>
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Distributer Name</label>
-            <input type="text" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">publisher Name</label>
-            <input type="text" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Price</label>
-            <input type="text" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-            <label htmlFor="" className="form-label">Edition</label>
-            <input type="text" className="form-control" />
-            </div>
-            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <button className="btn btn-success">Submit</button>
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value });
+    }
+    const readValue = () => {
+        console.log(data)
+        axios.post("https://courseapplogix.onrender.com/addstudents",data).then(
+            (Response)=>{
+                console.log(Response.data)
+                if (Response.data.status ==  "success") {
+                    alert("Successfully Added")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch(
+            (error)=>{
+                alert.apply(error.message)
+              }
+        )
+
+    }
+    
+    return (
+        <div>
+            <Navbar />
+            <h1><center>LIBRARY APP</center></h1>
+            
+            
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6">
+                                <label htmlFor="fname" className="form-label">BOOK TITLE</label>
+                                <input type="text" className="form-control" name=' bookTitle' value={data. bookTitle} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6">
+                                <label htmlFor="lname" className="form-label">Author</label>
+                                <input type="text" className="form-control" name='author' value={data.author} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6">
+                                <label htmlFor="college" className="form-label">PUBLISHED YEAR</label>
+                                <input type="text" className="form-control" name=' publishedyear' value={data. publishedyear} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6">
+                                <label htmlFor="dob" className="form-label">EDITION</label>
+                                <input type="date" className="form-control" name='edition' value={data.edition} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12">
+                                <label htmlFor="" className="form-label">CATEGORY</label>
+                                <select name='' className="form-control" value={data.category} onChange={inputHandler}>
+                                    <option value="">Select category</option>
+                                    <option value="FICTION">FICTION</option>
+                                    <option value="SCIENCE">SCIENCE</option>
+                                    <option value="HORROR">HORROR</option>
+                                   
+                                </select>
+                            </div>
+                            <div className="col col-12 col-sm-6">
+                                <label htmlFor="mob" className="form-label">PRICE</label>
+                                <input type="text" className="form-control" name='price' value={data.price} onChange={inputHandler} />
+                            </div>
+                            
+                            <div className="col col-12 col-sm-6">
+                                <button className="btn btn-success" onClick={readValue}>SUBMIT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
-        </div>
-    </div>
-</div>
-    </div>
-  )
+    )
 }
 
 export default AddBook
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,62 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
-const ViewAll = () => {
-    const [data, changeData] = useState(
-        [
-            { "title": "Alchemist", "Author": "paulo cohelo", "price": 999 },
-            { "title": "Halfgirlfriend", "Author": "chetan bhagat", "price": 555 },
-            { "title": "devdas", "Author": "sarat chandra", "price": 888 }
+const ViewBook = () => {
+    const [data, changeData] = useState([])
+    const fetchdata = () => {
+      axios.get("https://friendsapi-re5a.onrender.com/view").then((response)=>{
+        changeData(response.data)
+        console.log(response.data)
+      }).catch ((error)=>{
+        alert.apply(error.message)
+      })
+    }
 
-
-
-
-
-        ]
+    useEffect(
+      ()=>{fetchdata()},[]
     )
-
     return (
         <div>
             <Navbar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <table className="table">
+                        <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">title</th>
-                                    <th scope="col">Author</th>
-                                    
-                                    <th scope="col">price</th>
+                                    <th scope="col">BOOK TITLE </th>
+                                        <th scope="col">AUTHOR</th>
+                                        <th scope="col">PUBLISHED YEAR</th>
+                                        <th scope="col">EDITION</th>
+                                        <th scope="col">CATEGORY</th>
+                                        <th scope="col">PRICE</th>
+                                       
+                                        
+                                        
+                                        
                                 </tr>
                             </thead>
-
                             {data.map(
-                                (value,index)  => {
-                            return  <tbody>
-                                <tr>
-                                    <th scope="row">{value.title}</th>
-                                    <td>{value.Author}</td>
-                                    
-                                    <td>{value.price}</td>
-                                </tr>
+                                (value, index) => {
+                                    return <tbody>
+                                        <tr>
+                                            <th scope="row">{value.bookTitle}</th>
+                                            <td>{value.author}</td>
+                                            <td>{value.publishedYear}</td>
+                                            <td>{value.edition}</td>
+                                            <td>{value.category}</td>
+                                            <td>{value.price}</td>
+                                            
+                                            
+                                        </tr>
 
+                                    </tbody>
+                                }
+                            )}
 
-                            </tbody>
+                         </table>
 
-                         
-                        }
-                    )}
+                     </div>
+                 </div>
+             </div>
 
-
-                        </table>
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
+     </div>
     )
 }
 
-export default ViewAll
+export default ViewBook
